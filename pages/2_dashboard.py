6cef647350a0633a2f7385fdfb0252ecfe94d0e0
JacobBrown <jcb99mt@byui.edu>
2025-03-18 19:25:53 -0600

    pages

---
+++ pages/2_dashboard.py
@@ -0,0 +1,8 @@
+import streamlit as st
+# you need t import all libraies again in each page. 
+# unless you make a function that imports all the libraries on all pages
+
+
+st.write("dashboard")
+
+st.link_button("profile", url = "/profile")
